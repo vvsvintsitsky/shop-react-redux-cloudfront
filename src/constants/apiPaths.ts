f39e9f0f98d53e4143af74d07238fb317967c4d3
Uladzislau_Svintsitski <uladzislau_svintsitski@epam.com>
2022-07-17 19:46:00 +0200

    chore: update link to the product api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://t8phaezou2.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://jfrp9feove.execute-api.us-east-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
