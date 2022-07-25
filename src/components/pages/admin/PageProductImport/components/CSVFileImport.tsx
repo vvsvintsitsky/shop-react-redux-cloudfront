c16634f2eb0c36fde528fb5adb800a3042a2a90e
Uladzislau_Svintsitski <uladzislau_svintsitski@epam.com>
2022-07-25 12:01:33 +0200

    feat: add csv file uploading

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -39,8 +39,9 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
         }
       })
       console.log('File to upload: ', file.name)
-      console.log('Uploading to: ', response.data)
-      const result = await fetch(response.data, {
+      const { urlForUpload } = response.data
+      console.log('Uploading to: ', urlForUpload)
+      const result = await fetch(urlForUpload, {
         method: 'PUT',
         body: file
       })
